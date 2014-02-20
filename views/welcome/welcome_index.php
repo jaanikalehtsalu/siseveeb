1de165d2efe9a2bdfe612a9873f6c71199285110
malju <mallor.koor@khk.ee>
2014-02-20 22:38:48 +0200

    add table_filter (jc, css) change ruhmad

--- views/welcome/welcome_index.php
+++ views/welcome/welcome_index.php
@@ -1,5 +1,5 @@
-<h1>Welcome!</h1>
-<!-- Code for ajax
+<!--<h1>Welcome!</h1>
+Code for ajax
 <script type="text/javascript">
     function clickme() {
         $.post("welcome", $( "#ajax-form" ).serialize(), function (data) {
