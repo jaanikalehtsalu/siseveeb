9e8754fa1a25faf15c772c4254bddbc588232de8
Jaanika Lehtsalu <jaanika.lehtsalu@khk.ee>
2014-02-20 18:48:17 +0200

    Add dire and khk controllers and index files

---
+++ controllers/dire.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jannu
+ * Date: 20.02.14
+ * Time: 18:34
+ */
+class dire extends Controller {
+
+    function index(){
+    }
+}
---
+++ views/dire/dire_index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jannu
+ * Date: 20.02.14
+ * Time: 18:36
+ */ 
