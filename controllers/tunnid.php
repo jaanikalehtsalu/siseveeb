27cf5be11448748ca06e364f81479b813f1a872a
malju <mallor.koor@gmail.com>
2014-02-24 20:55:47 +0200

    Added tunnid in controllers. Added folder tunnid in views and tunnid_index in folder tunnid.

---
+++ controllers/tunnid.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: malju
+ * Date: 24.02.14
+ * Time: 20:23
+ */
+class tunnid extends Controller {
+
+    function index(){
+    }
+}
---
+++ views/tunnid/tunnid_index.php
@@ -0,0 +1,122 @@
+<div class="container">
+    <h1><small></small></h1>
+    <!--<div class="row">
+        <div class="col-md-6">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Developers</h3>
+                    <div class="pull-right">
+							<span class="clickable filter" data-toggle="tooltip" title="Toggle table filter" data-container="body">
+								<i class="glyphicon glyphicon-filter"></i>
+							</span>
+                    </div>
+                </div>
+                <div class="panel-body">
+                    <input type="text" class="form-control" id="dev-table-filter" data-action="filter" data-filters="#dev-table" placeholder="Filter Developers" />
+                </div>
+                <table class="table table-hover" id="dev-table">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Kilgore</td>
+                        <td>Trout</td>
+                        <td>kilgore</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Bob</td>
+                        <td>Loblaw</td>
+                        <td>boblahblah</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Holden</td>
+                        <td>Caulfield</td>
+                        <td>penceyreject</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>-->
+    <div class="col-md-6">
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                <h3 class="panel-title">Tunniplaan</h3>
+                <div class="pull-right">
+							<span class="clickable filter" data-toggle="tooltip" title="Toggle table filter" data-container="body">
+
+							</span>
+                </div>
+            </div>
+            <div class="panel-body">
+                <input type="text" class="form-control" id="task-table-filter" data-action="filter" data-filters="#task-table" placeholder="Otsing" />
+            </div>
+            <table class="table table-hover" id="task-table">
+                <thead>
+                <tr>
+                    <th></th>
+                    <th class="paev">Esmaspäev</th>
+                    <th class="paev">Teisipäev</th>
+                    <th class="paev">Kolmapäev</th>
+                    <th class="paev">Neljapäev</th>
+                    <th class="paev">Reede</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>08:30 <br />
+                        10:00</td>
+                    <td class="aine"></td>
+                    <td class="" "></td>
+                    <td>in progress</td>
+                </tr>
+                <tr>
+                    <td>10:15 <br />
+                        11:45</td>
+                    <td class="aine"></td>
+                    <td class="" "></td>
+                    <td>in progress</td>
+                </tr>
+                <tr>
+                    <td>11:55 <br />
+                        14:00</td>
+                    <td class="aine"></td>
+                    <td class="" "></td>
+                    <td>in progress</td>
+                </tr>
+                <tr>
+                    <td>14:10 <br />
+                        15:40</td>
+                    <td class="aine"></td>
+                    <td class="" "></td>
+                    <td>in progress</td>
+                </tr>
+                <tr>
+                    <td>15:45 <br />
+                        17:15</td>
+                    <td class="aine"></td>
+                    <td class="" "></td>
+                    <td>in progress</td>
+                </tr>
+                <tr>
+                    <td>18:55 <br />
+                        20:25</td>
+                    <td class="aine"></td>
+                    <td class="" "></td>
+                    <td>in progress</td>
+                </tr>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+</div>
