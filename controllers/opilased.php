804ce2889ebde41caf10d3a77203c8070f8e3545
Gertrk <gert.rk1@hotmail.com>
2014-03-02 18:36:17 +0200

    Update BUTTONS

---
+++ controllers/opilased.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gert
+ * Date: 2.03.14
+ * Time: 18:31
+ */
+class opilased extends Controller {
+
+    function index(){
+    }
+}
---
+++ views/opilased/opilased_index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gert
+ * Date: 2.03.14
+ * Time: 18:32
+ */ 
